513db8b75dd7685e839f2888756edf8e259d3f61
RyanMonday <rymonday@hotmail.com>
2013-12-14 15:42:04 -0800

    test level

---
+++ project/Scenes/RyansTests.scene.Layers/Relight_StaticLightingInfo.bat
@@ -0,0 +1,1 @@
+vLux.exe "" -project "C:\projects\ludum-dare-28\project" -tempfolder "C:\projects\ludum-dare-28\project\Scenes\RyansTests.scene.Layers" -out "Scenes\RyansTests" -quiet -minimized -config "C:\projects\ludum-dare-28\project\Scenes\RyansTests.scene.Layers\lighting.cfg"
