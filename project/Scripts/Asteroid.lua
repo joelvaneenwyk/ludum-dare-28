2b66a118d676bc5da7216fbb16a092c31b45f934
unknown <joel.van.eenwyk@JOEL-MX17.global.havok.com>
2013-12-15 16:40:39 -0800

    Added collision explosions, missile path tracking, and switched to using a collision from file.

--- project/Scripts/Asteroid.lua
+++ project/Scripts/Asteroid.lua
@@ -1,15 +1,39 @@
+--[[
+Author: Joel Van Eenwyk
+Purpose: Manages what happens when the player hits an asteroid or other asteroid
+--]]
+
 -- info fields: HitPoint, HitNormal, Force, RelativeVelocity,
 --              ColliderType, ColliderObject (maybe nil)
 function OnCollision(self, info)
-	for i, asteroid in pairs(G.asteroids) do
+	if G.asteroids == nil then
+		return
+	end
+
+	for _, asteroid in pairs(G.asteroids) do
 		-- if asteroids collide, then we move them away from each other
 		if self == asteroid.entity or info.ColliderObject == asteroid.rigidBody then
+			if info.ColliderObject == G.player then
+				Game:CreateEffect(
+					G.player:GetPosition(),
+					"Particles\\shipExplosion.xml",
+					"shipExplosion" )
+				Game:CreateEffect(
+					asteroid.entity:GetPosition(),
+					"Particles\\asteroidExplosion.xml",
+					"astroidExplosion" )
+				G.player:SetVisible(false)
+				G.player:SetPosition( Vision.hkvVec3(10000, 10000, 10000) )
+				G.Reset(2)
+			end
+
 			if asteroid.changeDirectionTimer > G.directionChangeTime then
-				local offset = asteroid.entity:GetPosition() - info.ColliderObject:GetPosition();
+				local offset = asteroid.entity:GetPosition() - info.ColliderObject:GetPosition()
 				offset:normalize()
+
 				asteroid.direction = offset
 				asteroid.changeDirectionTimer = 0
-			end				
+			end
 		end
 	end		
 end
--- project/Scripts/PlayerControl.lua
+++ project/Scripts/PlayerControl.lua
@@ -1,11 +1,10 @@
 --[[
-Author: Joel Van Eenwyk, Ryan Monday
+Author: Joel Van Eenwyk
 Purpose: Controls the player
 --]]
 
 function OnExpose(self)
 	self.MissileVelocity = 600
-	self.MissileFireTimer = 0.1
 	self.MissileScale = 0.2
 	self.MoveSpeed = 300
 	self.RotateSpeed = 5
@@ -40,32 +39,20 @@ function OnAfterSceneLoaded(self)
 			{ (G.screenWidth * 0.5), (G.screenHeight * 0.5), G.screenWidth, G.screenHeight },
 			"CT_TOUCH_ANY")
 	end
-	
-	self.roll = 0
-	self.missileFireTimer = 0
-	
-	self.position = self:GetPosition()
-	self.direction = Vision.hkvVec3(0, 1, 0)
-	self.speed = 0
-	self.missilePosition = Vision.hkvVec3(0, 0, 0)
-	self.missileDirection = Vision.hkvVec3(0, 0, 0)
-	self.missileBounces = 0
-	self.missileFired = false
-	
-	self.currentLevel = 0
-	self.asteroidCount = 3
 
-	G.asteroids = {}
-	G.directionChangeTime = 0.3
+	self:SetUseEulerAngles(true)
+
+	G.player = self
 	
-	self.asteroidTime = 0
 	local distance = 1600
 	local p1 = Screen:Project3D(0, 0, distance)
 	local p2 = Screen:Project3D(G.screenWidth, G.screenHeight, distance)
-	self.extentX = math.abs((p2.x - p1.x) / 2)
-	self.extentY = math.abs((p2.y - p1.y) / 2)
+	G.extentX = math.abs((p2.x - p1.x) / 2)
+	G.extentY = math.abs((p2.y - p1.y) / 2)
+		
+	G.Reset = Reset
+	G.Reset(0.0)
 
-	self:SetUseEulerAngles(true)
 end
 
 function OnBeforeSceneUnloaded(self)
@@ -79,6 +66,16 @@ end
 
 function OnThink(self)
 	local dt = Timer:GetTimeDiff()
+	
+	-- Handle resetting immediately as some things need to be initialized right away
+	if G.reset then
+		G.resetTime = G.resetTime - dt
+		if G.resetTime <= 0 then
+			HardReset()
+			G.reset = false
+		end
+	end
+	
 	local moveSpeed = self.MoveSpeed * dt
 	local rotateSpeed = self.RotateSpeed * dt
 	
@@ -86,20 +83,20 @@ function OnThink(self)
 	local rotate = 0
 
 	Debug.Draw:Line(
-		Vision.hkvVec3(-self.extentX, -self.extentY, 0),
-		Vision.hkvVec3(self.extentX, -self.extentY, 0) )
+		Vision.hkvVec3(-G.extentX, -G.extentY, 0),
+		Vision.hkvVec3(G.extentX, -G.extentY, 0) )
 	Debug.Draw:Line(
-		Vision.hkvVec3(-self.extentX, self.extentY, 0),
-		Vision.hkvVec3(self.extentX, self.extentY, 0) )
+		Vision.hkvVec3(-G.extentX, G.extentY, 0),
+		Vision.hkvVec3(G.extentX, G.extentY, 0) )
 	Debug.Draw:Line(
-		Vision.hkvVec3(-self.extentX, -self.extentY, 0),
-		Vision.hkvVec3(-self.extentX, self.extentY, 0) )
+		Vision.hkvVec3(-G.extentX, -G.extentY, 0),
+		Vision.hkvVec3(-G.extentX, G.extentY, 0) )
 	Debug.Draw:Line(
-		Vision.hkvVec3(self.extentX, -self.extentY, 0),
-		Vision.hkvVec3(self.extentX, self.extentY, 0) )
+		Vision.hkvVec3(G.extentX, -G.extentY, 0),
+		Vision.hkvVec3(G.extentX, G.extentY, 0) )
 
 	if IsTriggered(self, "KeyUp") or IsTriggered(self, "TouchUp") then
-		self.speed = self.speed + moveSpeed
+		G.speed = G.speed + moveSpeed
 	end
 
 	if IsTriggered(self, "KeyLeft") or IsTriggered(self, "TouchLeft") then
@@ -110,95 +107,125 @@ function OnThink(self)
 		rotate = rotate + rotateSpeed
 	end
 	
-	local right = self.direction:cross(Vision.hkvVec3(0, 0, 1))
+	local right = G.direction:cross(Vision.hkvVec3(0, 0, 1))
 	
-	self.direction = self.direction + right * rotate
-	self.direction:normalize()
+	G.direction = G.direction + right * rotate
+	G.direction:normalize()
 	
-	self.position = self.position + self.direction * self.speed
+	local position = self:GetPosition() + G.direction * G.speed
 	
-	if self.position.x > self.extentX then
-		self.position.x = self.extentX
-	elseif self.position.x < -self.extentX then
-		self.position.x = -self.extentX
+	if position.x > G.extentX then
+		position.x = G.extentX
+	elseif position.x < -G.extentX then
+		position.x = -G.extentX
 	end
 	
-	if self.position.y > self.extentY then
-		self.position.y = self.extentY
-	elseif self.position.y < -self.extentY then
-		self.position.y = -self.extentY
+	if position.y > G.extentY then
+		position.y = G.extentY
+	elseif position.y < -G.extentY then
+		position.y = -G.extentY
 	end
 	
-	self:SetPosition(self.position)
+	self:SetPosition(position)
 
 	local angle = 0
-	angle = math.atan2(self.direction.y, self.direction.x)
+	angle = math.atan2(G.direction.y, G.direction.x)
 	self:SetOrientation(math.deg(angle) - 90, 0, 0)
 
-	self.speed = self.speed * 0.9
+	G.speed = G.speed * 0.9
 		
 	if (IsTriggered(self, "KeyFire") or IsTriggered(self, "TouchFire")) then
-		self.missilePosition = self.position
-		self.missileDirection = self.direction
-		self.missileBounces = 0
-		self.missileFired = true
+		G.missilePosition = position
+		G.missileDirection = G.direction
+		G.missileBounces = 0
+		G.missileFired = true
+		G.missilePath = {}
+		table.insert(G.missilePath, G.missileDirection)
+		G.missileEffect = Game:CreateEffect(
+			G.missilePosition,
+			"Particles\\shipThruster.xml",
+			"astroidExplosion" )
 	end
 	
-	if self.missileFired then		
-		local rayStart = self.missilePosition
-		local rayEnd = rayStart + self.missileDirection * self.MissileVelocity * dt
+	if G.missileFired then		
+		local rayStart = G.missilePosition
+		local rayEnd = rayStart + G.missileDirection * 50
 		local iCollisionFilterInfo = Physics.CalcFilterInfo(Physics.LAYER_ALL, 0, 0, 0)
 		local isHit, result = Physics.PerformRaycast(rayStart, rayEnd, iCollisionFilterInfo)
 
-		local e = rayStart + self.missileDirection * 100
-		Debug.Draw:Line(rayStart, e)
+		local e = rayStart + G.missileDirectio;
 		Debug.Draw:Box(rayStart, 10)
+		Debug.Draw:Line(rayStart, rayEnd, Vision.V_RGBA_WHITE)
 		
 		if isHit == true then 
-			Debug.Draw:Line(rayStart, result["ImpactPoint"], Vision.V_RGBA_BLUE)
-			Debug:PrintLine("Hit type: " ..result["HitType"])
+			G.missilePosition = result.ImpactPoint
+			G.missileDirection = result.ImpactNormal
+			G.missileDirection.z = 0
+			G.missileDirection:normalize()
+			G.missileBounces = G.missileBounces + 1
+		
+			DeleteAsteroid(result.HitObject)
 
-			if result["HitType"] == "Entity" then
-				Debug:PrintLine("Mesh of hit entity: " ..result["HitObject"]:GetMesh():GetName())
-			end 
-
-			Debug:PrintLine("Hit fraction: " ..result["HitFraction"])
-			Debug:PrintLine("Hit position: " ..result["ImpactPoint"])
-			Debug:PrintLine("Hit normal: " ..result["ImpactNormal"])
-			Debug:PrintLine("Dynamic friction of hit object: " ..result["DynamicFriction"])
-			Debug:PrintLine("Restitution of hit object: " ..result["Restitution"])
-			Debug:PrintLine("Physics user data of hit object: " ..result["UserData"])
-
-			self.missilePosition = result["ImpactPoint"]
-			self.missileDirection = result["ImpactNormal"]
-			self.missileDirection.z = 0
-			self.missileDirection:normalize()
-			self.missileBounces = self.missileBounces + 1
+			Debug.Draw:Line(rayStart, result.ImpactPoint, Vision.V_RGBA_BLUE)
 		else
-			self.missilePosition = rayEnd
+			G.missilePosition = rayEnd
 		end
 		
-		if self.missilePosition.x > self.extentX then
-			self.missilePosition.x = self.extentX
-			self.missileDirection.x = -self.missileDirection.x
-			self.missileBounces = self.missileBounces + 1
-		elseif self.missilePosition.x < -self.extentX then
-			self.missilePosition.x = -self.extentX
-			self.missileDirection.x = -self.missileDirection.x
-			self.missileBounces = self.missileBounces + 1
-		elseif self.missilePosition.y > self.extentY then
-			self.missilePosition.y = self.extentY
-			self.missileDirection.y = -self.missileDirection.y
-			self.missileBounces = self.missileBounces + 1
-		elseif self.missilePosition.y < -self.extentY then
-			self.missilePosition.y = -self.extentY
-			self.missileDirection.y = -self.missileDirection.y
-			self.missileBounces = self.missileBounces + 1
+		if G.missilePosition.x > G.extentX then
+			G.missilePosition.x = G.extentX
+			G.missileDirection.x = -G.missileDirection.x
+			G.missileBounces = G.missileBounces + 1
+		elseif G.missilePosition.x < -G.extentX then
+			G.missilePosition.x = -G.extentX
+			G.missileDirection.x = -G.missileDirection.x
+			G.missileBounces = G.missileBounces + 1
+		elseif G.missilePosition.y > G.extentY then
+			G.missilePosition.y = G.extentY
+			G.missileDirection.y = -G.missileDirection.y
+			G.missileBounces = G.missileBounces + 1
+		elseif G.missilePosition.y < -G.extentY then
+			G.missilePosition.y = -G.extentY
+			G.missileDirection.y = -G.missileDirection.y
+			G.missileBounces = G.missileBounces + 1
 		end
 	end
 	
 	UpdateAsteroids(self)
 end
+
+--== Global game utility functions
+
+function Reset(delay)
+	G.missileFired = false
+
+	G.reset = true
+	G.resetTime = delay
+end
+
+function HardReset()
+	DeleteAsteroids()
+
+	G.missileFireTimer = 0
+	
+	G.direction = Vision.hkvVec3(0, 1, 0)
+	G.speed = 0
+	G.missilePosition = Vision.hkvVec3(0, 0, 0)
+	G.missileDirection = Vision.hkvVec3(0, 0, 0)
+	G.missileBounces = 0
+	G.missilePath = {}
+	G.missileFireTimer = 0.1
+
+	G.currentLevel = 0
+	G.asteroidCount = 3
+	G.asteroidTime = 0
+
+	G.directionChangeTime = 0.3
+
+	G.player:SetPosition( Vision.hkvVec3(0, 0, 0) )
+	G.player:SetVisible(true)
+end
+
+--== Asteroid helper functions
 
 function UpdateAsteroids(self)
 	local dt = Timer:GetTimeDiff()
@@ -207,17 +234,17 @@ function UpdateAsteroids(self)
 		local position = asteroid.entity:GetPosition()
 		position = position + asteroid.direction * asteroid.speed * dt
 		
-		if position.x > self.extentX then
-			position.x = self.extentX
+		if position.x > G.extentX then
+			position.x = G.extentX
 			asteroid.direction.x = -asteroid.direction.x
-		elseif position.x < -self.extentX then
-			position.x = -self.extentX
+		elseif position.x < -G.extentX then
+			position.x = -G.extentX
 			asteroid.direction.x = -asteroid.direction.x
-		elseif position.y > self.extentY then
-			position.y = self.extentY
+		elseif position.y > G.extentY then
+			position.y = G.extentY
 			asteroid.direction.y = -asteroid.direction.y
-		elseif position.y < -self.extentY then
-			position.y = -self.extentY
+		elseif position.y < -G.extentY then
+			position.y = -G.extentY
 			asteroid.direction.y = -asteroid.direction.y
 		end
 		
@@ -229,24 +256,46 @@ function UpdateAsteroids(self)
 		asteroid.changeDirectionTimer = asteroid.changeDirectionTimer + dt
 	end
 	
-	self.asteroidTime = self.asteroidTime + dt
-	if self.asteroidTime > Util:GetRandFloat(2) + 0.5 and
-	   table.getn(G.asteroids) < self.asteroidCount then
+	G.asteroidTime = G.asteroidTime + dt
+	if G.asteroidTime > Util:GetRandFloat(2) + 0.5 and
+	   table.getn(G.asteroids) < G.asteroidCount then
 		CreateAsteroid(self)
-		self.asteroidTime = 0
+		G.asteroidTime = 0
 	end
 end
 
-function DeleteAsteroids(self)
-	for i, asteroid in pairs(G.asteroids) do
-		asteroid.entity:Remove()
+function DeleteAsteroid(asteroidToDelete)
+	if G.asteroids then
+		for asteroidIndex, asteroid in pairs(G.asteroids) do
+			if asteroid.entity == asteroidToDelete then
+				Game:CreateEffect(
+					asteroid.entity:GetPosition(),
+					"Particles\\asteroidExplosion.xml",
+					"asteroidExplosion" )
+
+				asteroid.entity:Remove()
+				table.remove(G.asteroids, asteroidIndex)
+				break
+			end
+		end
 	end
-	asteroids = {}
 end
 
-function CreateAsteroid(self)
-	local position = Vision.hkvVec3(0, 0, 0)
-	local model = "Models/asteroid01.model"
+function DeleteAsteroids()
+	if G.asteroids then
+		for _, asteroid in pairs(G.asteroids) do
+			asteroid.entity:Remove()
+		end
+	end
+
+	G.asteroids = {}
+end
+
+function CreateAsteroid()
+	local position = Vision.hkvVec3(0, 0, -5.0)
+	local variation = 4 --Util:GetRandInt(3) + 1
+	local model = "Models/asteroid0" .. variation .. ".model"
+	local collision = "Models/asteroid_collision.hkt"
 
 	local asteroid = {}
 	
@@ -254,28 +303,29 @@ function CreateAsteroid(self)
 	
 	if Util:GetRandFloat() > 0.5 then
 		if Util:GetRandFloat() > 0.5 then
-			position.x = self.extentX * -1
+			position.x = G.extentX * -1
 			asteroid.direction.x = 1
 		else
-			position.x = self.extentX
+			position.x = G.extentX
 			asteroid.direction.x = -1
 		end
 		asteroid.direction.y = Util:GetRandFloat(2) - 1
-		position.y = Util:GetRandFloat(self.extentY)
+		position.y = Util:GetRandFloat(G.extentY)
 	else
 		if Util:GetRandFloat() > 0.5 then
-			position.y = self.extentY * -1
+			position.y = G.extentY * -1
 			asteroid.direction.y = 1
 		else
-			position.y = self.extentY
+			position.y = G.extentY
 			asteroid.direction.y = -1
 		end	
 		asteroid.direction.x = Util:GetRandFloat(2) - 1
-		position.x = Util:GetRandFloat(self.extentX)
+		position.x = Util:GetRandFloat(G.extentX)
 	end
+
 	asteroid.direction:normalize()
 	
-	asteroid.scale = Util:GetRandFloat(1) + 1
+	asteroid.scale = Util:GetRandFloat(2) + 1.5
 	asteroid.entity = Game:CreateEntity(
 		position,
 		"VisBaseEntity_cl",
@@ -286,21 +336,22 @@ function CreateAsteroid(self)
 	asteroid.rotation = 0
 
 	asteroid.rigidBody = asteroid.entity:AddComponentOfType("vHavokRigidBody")
+	asteroid.rigidBody:SetDebugRendering(true)
 
-	asteroid.rigidBody:InitCylinder(
-		Vision.hkvVec3(0, 0, 10),
-		Vision.hkvVec3(0, 0, -10),
-		50,
-		asteroid.scale,
-		Physics.MOTIONTYPE_KEYFRAMED,
-		Physics.QUALITY_KEYFRAMED_REPORTING)
+	local aabb = asteroid.entity:GetCollisionBoundingBox()	
+	local radius = math.max(aabb:getSizeX(), aabb:getSizeY()) / 2.0
+	local success = asteroid.rigidBody:InitFromFile(collision, radius / 100.0)
 
-	asteroid.entity:AddComponentOfType("VScriptComponent", "AsteroidControlScript")
-	asteroid.entity.AsteroidControlScript:SetProperty("ScriptFile", "Scripts/Asteroid.lua")
-	asteroid.entity.AsteroidControlScript:SetOwner(asteroid.entity)
-	
-	asteroid.speed = Util:GetRandFloat(100) + 100
-	asteroid.changeDirectionTimer = 0
+	if success then
+		asteroid.entity:AddComponentOfType("VScriptComponent", "AsteroidControlScript")
+		asteroid.entity.AsteroidControlScript:SetProperty("ScriptFile", "Scripts/Asteroid.lua")
+		asteroid.entity.AsteroidControlScript:SetOwner(asteroid.entity)
+		
+		asteroid.speed = Util:GetRandFloat(100) + 100
+		asteroid.changeDirectionTimer = 0
 
-	table.insert(G.asteroids, asteroid)
+		table.insert(G.asteroids, asteroid)
+	else
+		asteroid.entity:Remove()
+	end
 end
